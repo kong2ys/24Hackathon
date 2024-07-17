b008320ab56ab130b418bcc68f326e929223a0c5
kong2ys <yousung0525@naver.com>
2024-07-17 10:10:06 +0900

    feat :: SoundManager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,9 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    
+    #region Singleton
+
+    static public SoundManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    #endregion
+
     public enum Sfx
     {
         jump,
@@ -22,12 +43,64 @@ public class SoundManager : MonoBehaviour
     
     void Start()
     {
-        GameObject bgmObject
+        #region bgmPlayer Initalize
+
+        GameObject bgmObject = new GameObject("BgmPlayer");
+        bgmObject.transform.parent = transform;
+        bgmPlayer = bgmObject.AddComponent<AudioSource>();
+        bgmPlayer.playOnAwake = false;
+        bgmPlayer.loop = true;
+        //bgmPlayer.volume
+        bgmPlayer.clip = bgmClip;
+        
+        #endregion
+
+        #region sfxPlayer Initalize
+
+        GameObject sfxObject = new GameObject("SfxPlayer");
+        sfxObject.transform.parent = transform;
+        sfxPlayers = new AudioSource[channels];
+
+        for (int i = 0; i < sfxPlayers.Length; i++)
+        {
+            sfxPlayers[i] = sfxObject.AddComponent<AudioSource>();
+            sfxPlayers[i].playOnAwake = false;
+            //sfxPlayers[i].volume
+        }
+
+        #endregion
+        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayBgm(bool isPlay)
     {
-        
+        if (isPlay)
+        {
+            bgmPlayer.Play();
+        }
+        else
+        {
+            bgmPlayer.Stop();
+        }
     }
+
+    public void PlaySfx(Sfx sfx)
+    {
+        for (int i = 0; i < sfxPlayers.Length; i++)
+        {
+            int loopIndex = (i + channelIndex) % sfxPlayers.Length;
+
+            if (sfxPlayers[loopIndex].isPlaying)
+            {
+                continue;
+            }
+
+            channelIndex = loopIndex;
+            sfxPlayers[loopIndex].clip = sfxClips[(int)sfx];
+            sfxPlayers[loopIndex].Play();
+            break;
+        }
+    }
+    
 }
