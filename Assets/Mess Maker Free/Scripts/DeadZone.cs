e3154e3c58895058e6d926153e7cbc50a165c36a
kong2ys <yousung0525@naver.com>
2024-07-17 15:29:04 +0900

    feat  :: ground collider

---
+++ Assets/Mess Maker Free/Scripts/DeadZone.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("닿음");
+        }
+    }
+}
